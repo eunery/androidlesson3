8707da9fd977ddd444d508f741e16af26109fd99
eunery <61619523+eunery@users.noreply.github.com>
2022-03-14 22:34:24 +0500

    Update UserData.kt

--- app/src/main/java/com/example/androidlesson3/cabinet/UserData.kt
+++ app/src/main/java/com/example/androidlesson3/cabinet/UserData.kt
@@ -2,9 +2,9 @@ package com.example.androidlesson3.cabinet
 
 data class UserData(
     val user: User = User(names = "Иванов Иван Иванович", address = "Сахалин, ул. Пушкина, д. Колотушкина"),
-    val balanceData : Balance = Balance(id = 1726872, amount = 182.21, toPay = 0.0),
+    val balanceData : Balance = Balance(id = 1726872, amount = 1337.228, toPay = 10.0),
     val tariffs : List<Tariff> = mutableListOf(
-        Tariff(id = 1, name = "Улыбка (бесплатный тариф)", description = "Скорость до 100 Мбит/c", amount = 0.0),
-        Tariff(id = 2, name = "Победа", description = "Скорость до 350 Мбит/c", amount = 400.0)
+        Tariff(id = 1, name = "Жаба (бесплатный тариф)", description = "Скорость до 15! Мбит/c", amount = 0.0),
+        Tariff(id = 2, name = "Сосиска", description = "Скорость до 0.0001 Мбит/c", amount = 001.0)
     )
 )
